166138a5aa1e25e870f365de50c06ec974811fa7
Kevin Barabash <kevinb7@gmail.com>
2012-11-10 15:14:01 -0700

    First attempt to get the jasmine adapter working.  No messages are reaching server.js unfortunately.

--- client/adapters/jasmine.js
+++ client/adapters/jasmine.js
@@ -3,7 +3,8 @@
 		return;
 	}
 
-	var TesteeReporter = function() {
+    var socket = io.connect();
+    var TesteeReporter = function() {
 
 	}
 
@@ -13,23 +14,67 @@
 		},
 
 		reportRunnerStarting : function(runner) {
+            socket.emit("start", {
+                environment : navigator.userAgent,
+                runner : 'Jasmine'
+            });
 
+            runner.suites.forEach(function (suite) {
+                if (suite.parentSuite !== null) {
+                    socket.emit('suite', {
+                        "title": suite.description,
+                        "parent": suite.parentSuite.id,
+                        "id": suite.id
+                    });
+                } else {
+                    socket.emit('suite', {
+                        "title": suite.description,
+                        "root": true,
+                        "id": suite.id
+                    });
+                }
+            });
 		},
 
 		reportRunnerResults : function(runner) {
-
+            socket.emit("end", {});
 		},
 
 		reportSpecResults : function(spec) {
+            if (spec.results_.failedCount) {
+                var message = spec.results_.items_[0].message;
+                var stack = spec.results_.items_[0].trace.stack;
+                socket.emit("fail", {
+                    "id": spec.id,
+                    "err": {
+                        "message": message,
+                        "stack": stack
+                    }
+                });
+            } else if (spec.results_.passedCount) {
+                socket.emit("pass", {
+                    "duration": 0,
+                    "id": spec.id
+                });
+            }
 
+            socket.emit("test end", {
+                "id": spec.id
+            });
 		},
 
 		reportSpecStarting : function(spec) {
-
+            socket.emit("test", {
+                "title": spec.description,
+                "parent": spec.suite.id,
+                "id": spec.id
+            })
 		},
 
 		reportSuiteResults : function(suite) {
-
+            socket.emit("suite end", {
+                "id": suite.id
+            });
 		}
 	});
 
--- client/dist/testee.js
+++ client/dist/testee.js
@@ -1406,7 +1406,8 @@ Testee._ = _.noConflict();
 		return;
 	}
 
-	var TesteeReporter = function() {
+    var socket = io.connect();
+    var TesteeReporter = function() {
 
 	}
 
@@ -1416,23 +1417,62 @@ Testee._ = _.noConflict();
 		},
 
 		reportRunnerStarting : function(runner) {
-
+            runner.suites.forEach(function (suite) {
+                if (suite.parentSuite !== null) {
+                    socket.emit('suite', {
+                        "title": suite.description,
+                        "parent": suite.parentSuite.id,
+                        "id": suite.id
+                    });
+                } else {
+                    socket.emit('suite', {
+                        "title": suite.description,
+                        "root": true,
+                        "id": suite.id
+                    });
+                }
+            });
 		},
 
 		reportRunnerResults : function(runner) {
-
+            socket.emit("end", {});
 		},
 
 		reportSpecResults : function(spec) {
+            if (spec.results_.failedCount) {
+                var message = spec.results_.items_[0].message;
+                var stack = spec.results_.items_[0].trace.stack;
+                socket.emit("fail", {
+                    "id": spec.id,
+                    "err": {
+                        "message": message,
+                        "stack": stack
+                    }
+                });
+            } else if (spec.results_.passedCount) {
+                socket.emit("pass", {
+                    "duration": 0,
+                    "id": spec.id
+                });
+            }
 
+            socket.emit("test end", {
+                "id": spec.id
+            });
 		},
 
 		reportSpecStarting : function(spec) {
-
+            socket.emit("test", {
+                "title": spec.description,
+                "parent": spec.suite.id,
+                "id": spec.id
+            })
 		},
 
 		reportSuiteResults : function(suite) {
-
+            socket.emit("suite end", {
+                "id": suite.id
+            });
 		}
 	});
 
