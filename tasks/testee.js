6156ae2545e13b28f61adfd58020d47b382166db
David Luecke <daff@neyeon.com>
2012-11-26 22:58:20 -0700

    A working Grunt Task

--- tasks/testee.js
+++ tasks/testee.js
@@ -1,13 +1,18 @@
 var testee = require('../lib/testee');
 
 module.exports = function(grunt) {
-	grunt.registerTask('testee', 'Run tests', function() {
+	grunt.registerMultiTask('testee', 'Run tests', function() {
 		var configuration = grunt.config(['testee', this.target]);
 		var files = grunt.file.expand(this.file.src);
-		testee.test(files, configuration, function (error, results) {
+		var done = this.async();
+		testee.test(files, configuration, function (error, stats) {
 			if(error) {
-				return grunt.fail(err);
+				return grunt.warn(err);
 			}
+			if(stats.failed) {
+				return grunt.warn(stats.failed + ' test(s) failed');
+			}
+			done();
 		});
 	});
 }
