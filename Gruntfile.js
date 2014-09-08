2a02dc92f2260732a4bc89268f27e9c79c23cc91
David Luecke <daff@neyeon.com>
2014-09-08 09:04:10 -0600

    Error gracefully. Fixing Grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,16 +5,6 @@ module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			adapters: 'client/adapters',
-			dist: 'client/dist',
-			components: 'client/components',
-			banner: '/*!\n * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-				'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-				'<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
-				' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
-				' * Licensed <%= _.pluck(pkg.licenses, "name").join(", ") %>\n */\n'
-		},
 		jshint: {
 //			options: {
 //				jshintrc: {
@@ -38,72 +28,25 @@ module.exports = function (grunt) {
 //			},
 			lib: [ 'lib/**/*.js' ]
 		},
-		concat: {
-			options: {
-				stripBanners: true,
-				banner: '<%= meta.banner %>'
-			},
-			adapters: {
-				src: [
-					'<%= meta.components %>/underscore/underscore.js',
-					'<%= meta.adapters %>/index.js',
-					'<%= meta.adapters %>/mocha.js',
-					'<%= meta.adapters %>/qunit.js',
-					'<%= meta.adapters %>/jasmine.js',
-					'<%= meta.adapters %>/bootstrap.js'
-				],
-				dest: '<%= meta.dist %>/<%= pkg.name %>.js'
-			}
-		},
-		uglify: {
-			options: {
-				banner: '<%= meta.banner %>'
-			},
-			adapters: {
-				files: {
-					'<%= meta.dist %>/<%= pkg.name %>.min.js': '<%= meta.dist %>/<%= pkg.name %>.js'
-				}
-			}
-		},
-		testee: {
-			local: {
-				options: {
-					urls: ['client/examples/qunit/index.html'],
-					browsers: ['ie']
-				}
-			},
 
-			win7: {
-				options: {
-					port: 4040,
-					urls: ['http://pivotal.github.io/jasmine/'],
-					browsers: ['ie'],
-					tunnel: {
-						"type": "local",
-						"hostname": "192.168.2.100"
-					},
-					"launch": {
-						"type": "remote",
-						"host": "192.168.179.129",
-						port: 8080,
-						"username": "launcher",
-						"password": "testing"
-					}
-				}
-			}
-		},
-    release: {}
+    release: {},
+
+    testee: {
+      options: {
+        reporter: 'Spec'
+      },
+      examples: [
+        'examples/mocha/index.html',
+        'examples/qunit/index.html',
+        'examples/jasmine/index.html'
+      ]
+    }
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-release');
 
 	grunt.loadTasks('tasks');
 
-	grunt.registerTask('adapters', [ 'concat', 'uglify' ]);
 	// Default task.
 	grunt.registerTask('default', 'jshint');
-
 };
--- tasks/testee.js
+++ tasks/testee.js
@@ -1,46 +1,17 @@
 var testee = require('../lib/testee');
-var async = require('async');
-var extend = require('util')._extend;
-var _ = require('underscore');
 
 module.exports = function (grunt) {
   grunt.registerMultiTask('testee', 'Run tests', function () {
     var done = this.async();
+    var options = this.options();
+    var files = this.filesSrc;
+    var browsers = options.browsers || ['phantom'];
 
-    var opts = this.options({
-      urls: [],
-      browsers: []
+    testee.test(files, browsers, options).then(function() {
+      done();
+    }, function(errors) {
+      grunt.fail(errors);
+      done(errors);
     });
-
-    var workers = _.map(opts.browsers, function (browser) {
-      var testeeOptions = _.extend({
-        browser: browser
-      }, _.omit(opts, 'browsers', 'urls'));
-
-      return function (callback) {
-        grunt.log.writeln('Running testee on ', browser);
-        testee.test(opts.urls[0], testeeOptions, callback);
-      }
-    });
-
-    var failOrPass = function (err, results) {
-      if (results) {
-        var resultsPerBrowser = _.map(results, function (result) {
-          return (result && result[0]) || {};
-        });
-        var failuresPerBrowser = _.pluck(resultsPerBrowser, 'failed');
-        var failureCount = _.reduce(failuresPerBrowser, function (total, failed) {
-          return total + failed;
-        }, 0);
-        done(failureCount === 0);
-      } else {
-        console.error(err);
-        console.error(err.stack);
-        done(err);
-      }
-    };
-
-    async.series(workers, failOrPass);
-
   });
-}
+};
