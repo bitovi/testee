a909fbbda0d20282a207573718e7bf85198f7798
Curtis Cummings <curtis@pixelplicity.com>
2016-09-28 10:35:48 -0400

    Making REST provider work (#85)
    - body-parser added to parse JSON service calls
    - tests updated to run with socketio and rest

--- test/coverage.test.js
+++ test/coverage.test.js
@@ -16,41 +16,84 @@ describe('Coverage', function() {
     }
   };
   
-  it('QUnit example', function(done) {
-    del([ 'test/coverage' ]).then(function () {
-      testee.test([ 'examples/qunit/index.html' ], [ 'firefox' ], config).fail(function(error) {
-        assert.equal(error.message, 'There were 0 general errors and 1 total test failures.');
+  describe('QUnit example', function() {
+    it('socketio', function(done) {
+      del([ 'test/coverage' ]).then(function () {
+        testee.test([ 'examples/qunit/index.html' ], [ 'firefox' ], config).fail(function(error) {
+          assert.equal(error.message, 'There were 0 general errors and 1 total test failures.');
 
-        async.map([ 'test/coverage/__root__', 'test/coverage/qunit' ], fs.readdir, function (err, files) {
+          async.map([ 'test/coverage/__root__', 'test/coverage/qunit' ], fs.readdir, function (err, files) {
+              assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
+              assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of qunit/test.js');
+              done();
+          });
+        });
+      });
+    });
+    it('rest', function(done) {
+      del([ 'test/coverage' ]).then(function () {
+        testee.test([ 'examples/qunit/rest.html' ], [ 'firefox' ], config).fail(function(error) {
+          assert.equal(error.message, 'There were 0 general errors and 1 total test failures.');
+
+          async.map([ 'test/coverage/__root__', 'test/coverage/qunit' ], fs.readdir, function (err, files) {
+              assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
+              assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of qunit/test.js');
+              done();
+          });
+        });
+      });
+    });
+  });
+
+  describe('Jasmine example', function() {
+    it('socketio', function(done) {
+      del([ 'test/coverage' ]).then(function () {
+        testee.test([ 'examples/jasmine/index.html' ], [ 'firefox' ], config).fail(function(error) {
+          assert.equal(error.message, 'There were 0 general errors and 1 total test failures.');
+
+          async.map([ 'test/coverage/__root__', 'test/coverage/jasmine' ], fs.readdir, function (err, files) {
             assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
-            assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of qunit/test.js');
+            assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of jasmine/test.js');
             done();
+          });
+        });
+      });
+    });
+    it('rest', function(done) {
+      del([ 'test/coverage' ]).then(function () {
+        testee.test([ 'examples/jasmine/rest.html' ], [ 'firefox' ], config).fail(function(error) {
+          assert.equal(error.message, 'There were 0 general errors and 1 total test failures.');
+
+          async.map([ 'test/coverage/__root__', 'test/coverage/jasmine' ], fs.readdir, function (err, files) {
+            assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
+            assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of jasmine/test.js');
+            done();
+          });
         });
       });
     });
   });
 
-  it('Jasmine example', function(done) {
-    del([ 'test/coverage' ]).then(function () {
-      testee.test([ 'examples/jasmine/index.html' ], [ 'firefox' ], config).fail(function(error) {
-        assert.equal(error.message, 'There were 0 general errors and 1 total test failures.');
-
-        async.map([ 'test/coverage/__root__', 'test/coverage/jasmine' ], fs.readdir, function (err, files) {
-          assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
-          assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of jasmine/test.js');
-          done();
+  describe('Mocha example', function() {
+    it('socketio', function(done) {
+      del([ 'test/coverage' ]).then(function () {
+        testee.test([ 'examples/mocha/index.html' ], [ 'firefox' ], config).fail(function() {
+          async.map([ 'test/coverage/__root__', 'test/coverage/mocha' ], fs.readdir, function (err, files) {
+            assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
+            assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of mocha/test.js');
+            done();
+          });
         });
       });
     });
-  });
-
-  it('Mocha example', function(done) {
-    del([ 'test/coverage' ]).then(function () {
-      testee.test([ 'examples/mocha/index.html' ], [ 'firefox' ], config).fail(function() {
-        async.map([ 'test/coverage/__root__', 'test/coverage/mocha' ], fs.readdir, function (err, files) {
-          assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
-          assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of mocha/test.js');
-          done();
+    it('rest', function(done) {
+      del([ 'test/coverage' ]).then(function () {
+        testee.test([ 'examples/mocha/rest.html' ], [ 'firefox' ], config).fail(function() {
+          async.map([ 'test/coverage/__root__', 'test/coverage/mocha' ], fs.readdir, function (err, files) {
+            assert.ok(files[0].indexOf('blogpost.js.html') >= 0, 'reports coverage of blogpost.js');
+            assert.ok(files[1].indexOf('test.js.html') >= 0, 'reports coverage of mocha/test.js');
+            done();
+          });
         });
       });
     });
