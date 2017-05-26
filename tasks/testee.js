7bbd009dcf655d67a20061e95a0afc209344c9ac
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-05-26 11:15:50 -0400

    Deprecate grunt task (#133)

--- tasks/testee.js
+++ tasks/testee.js
@@ -1,33 +1,7 @@
-var path = require('path');
-var testee = require('../lib/testee');
-
-module.exports = function (grunt) {
-  grunt.registerMultiTask('testee', 'Run tests', function () {
-    var done = this.async();
-    var options = this.options();
-    var browsers = options.browsers || ['phantom'];
-    var files = grunt.file.expand(grunt.util._.flatten(this.files.map(function (file) {
-      // Test file path need to be absolute to the root so that grunt.file.expand can glob them
-      return file.orig.src;
-    })).map(function(filename) {
-      return path.join(options.root || '', filename);
-    }));
-
-    if(options.root) {
-      // Each file in the globbed list needs to be converted back to their relative paths
-      files = files.map(function (file) {
-        return path.relative(options.root || '', file);
-      });
-    }
-
-    if(!files.length) {
-      return done(new Error('No file passed to Testee task.'));
-    }
-
-    testee.test(files, browsers, options).then(function () {
-      done();
-    }, function (error) {
-      done(error);
-    });
-  });
-};
+console.warn([
+  'DEPRECATION:',
+  'The Testee Grunt task has been moved to its own project and will be removed in testee@0.7.0.',
+  '\nTo migrate, please run `npm install --save-dev grunt-testee`',
+  'and replace `grunt.loadNpmTasks("testee");` with `grunt.loadNpmTasks("grunt-testee");`.',
+].join(' '))
+module.exports = require('grunt-testee');
