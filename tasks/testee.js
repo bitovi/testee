21b44434b500b807370337ee340ae298c89cc117
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-07-10 10:56:36 -0400

    Remove grunt task

--- tasks/testee.js
+++
@@ -1,7 +0,0 @@
-console.warn([
-  'DEPRECATION:',
-  'The Testee Grunt task has been moved to its own project and will be removed in testee@0.7.0.',
-  '\nTo migrate, please run `npm install --save-dev grunt-testee`',
-  'and replace `grunt.loadNpmTasks("testee");` with `grunt.loadNpmTasks("grunt-testee");`.',
-].join(' '))
-module.exports = require('grunt-testee');
